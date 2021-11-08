c16d158f44af4d056328d026fd923b000684e06b
hykare <78385964+hykare@users.noreply.github.com>
2021-11-08 21:25:21 +0100

    uses max_by to get key,value pair with highest value

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,19 +1,12 @@
 def stock_picker(array)
   profits = {}
 
-for i in 0..array.length-1 do 
-    for j in i..array.length-1 do
-        profits[[i,j]]=array[j]-array[i]
+  (0..array.length - 1).each do |i|
+    (i..array.length - 1).each do |j|
+      profits[[i, j]] = array[j] - array[i]
     end
+  end
+
+  profits.max_by { |_k, v| v }[0]
 end
-
-max = profits.reduce(profits.values[0]) do |accumulator, (k,v)|
-    v>accumulator ? v : accumulator
-end
-
-p profits.select {|k, v| v==max}.keys[0]
-
-
-end
-
-p stock_picker([17, 3, 3, 9, 15, 8, 6, 11, 1])
+p stock_picker([17, 3, 3, 9, 15, 18, 6, 11, 1])
